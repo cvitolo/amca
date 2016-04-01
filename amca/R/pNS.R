9449c2202c9a09e052512f8f6fc9f1f5b4bbdbd5
Claudia Vitolo <claudia.vitolo@gmail.com>
2016-04-01 15:35:48 +0100

    Added probabilistic Nash-Sutcliffe

---
+++ amca/R/pNS.R
@@ -0,0 +1,24 @@
+#' Probabilistic Nash-Sutcliffe efficiency
+#'
+#' @description This is an implementation of the probabilistic Nash-Sutcliffe efficiency defined by Bulygina et al. (2009).
+#'
+#' @param Qobs vector of observed values
+#' @param discharges is a matrix of simulations
+#'
+#' @return probabilistic efficiency
+#'
+#' @examples
+#' # pNS(Qobs, discharges)
+#'
+
+pNS <- function (Qobs, discharges){
+
+  meanD <- apply(discharges, 2, mean)
+  varD <- apply(discharges, 2, var)
+
+  EF <- ( 1 - sum((meanD - Qobs)^2)/sum((Qobs - mean(Qobs))^2) ) -
+    ( sum(varD) / sum((Qobs - mean(Qobs))^2)^2 )
+
+  return(EF)
+
+}
